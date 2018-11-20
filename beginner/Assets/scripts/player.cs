43d5898cd5f13689a581eb9067c950171dd41eb7
Augusto Calaca <augusto@Calaca.Augusto>
2018-11-20 01:10:16 -0300

    Add lives, rings, button pausea and menu in canvas

--- beginner/Assets/scripts/player.cs
+++ beginner/Assets/scripts/player.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class player : MonoBehaviour {
   public float forceSalt;
   public float velocityFactor;
-	void Start () {
 
-	}
+	void Start () { }
 
-	// Update is called once per frame
 	void Update () {
     Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
-    float move = Input.GetAxis("Horizontal");
-    bool keyDownSpace = Input.GetKeyDown(KeyCode.Space);
     rigidbody.velocity = new Vector2(move * velocityFactor, rigidbody.velocity.y);
+
+    float move = Input.GetAxis("Horizontal");
     if(move < 0)
       GetComponent<SpriteRenderer>().flipX = true;
     else if(move > 0)
       GetComponent<SpriteRenderer>().flipX = false;
+
+    if(move < 0 || move > 0)
+      GetComponent<Animator>().SetBool("walking", true);
+    else
+      GetComponent<Animator>().SetBool("walking", false);
+
+    bool keyDownSpace = Input.GetKeyDown(KeyCode.Space);
     if(keyDownSpace) {
       rigidbody.AddForce(new Vector2(0, forceSalt));
       GetComponent<Animator>().SetBool("jumped", true);
@@ -27,10 +32,5 @@ public class player : MonoBehaviour {
     if(!keyDownSpace && GetComponent<Animator>().GetBool("jumped"))
       GetComponent<Animator>().SetBool("jumped", false);
 
-
-    if(move < 0 || move > 0)
-      GetComponent<Animator>().SetBool("walking", true);
-    else
-      GetComponent<Animator>().SetBool("walking", false);
   }
 }
