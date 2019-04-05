0a0e0f9683863e1f7038ab29d8ddd02fdc7d55a3
AugustoCalaca <augustocalaca2@gmail.com>
2019-04-04 21:25:06 -0300

    add script spawning food

---
+++ snake/Assets/Scripts/SpawningFood.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningFood : MonoBehaviour {
+  public GameObject food;
+  public Transform top;
+  public Transform bottom;
+  public Transform left;
+  public Transform right;
+  void Start() {
+  
+  }
+
+  void Spawn() {
+    int x = (int) Random.Range(left.position.x, right.position.x);
+    int y = (int) Random.Range(bottom.position.y, top.position.y);
+
+    Instantiate(food, new Vector2(x, y), Quaternion.identity);
+  }
+
+  void OnTriggerEnter2D(Collider2D other) {
+    if(other.gameObject.CompareTag("Head")) {
+      Spawn();
+    }
+  }
+}
