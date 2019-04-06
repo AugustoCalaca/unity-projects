fd2c195551be090d30d6fcc70662ab416c653a59
AugustoCalaca <augustocalaca2@gmail.com>
2019-04-06 19:18:07 -0300

    add pontuation and gamer over case collider with tail or border

--- snake/Assets/Scripts/Snake.cs
+++ snake/Assets/Scripts/Snake.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Snake : MonoBehaviour {
   List<Transform> tail = new List<Transform>();
   public GameObject tailPrefab;
+  public GameObject panel;
   Vector2 direction;
+  public Text showPoints;
   bool eat;
+  int points, plus;
+
   void Start() {
-    InvokeRepeating("Move", 0.3f, 0.1f);
     direction = Vector2.up;
     eat = false;
+    points = 0;
+    plus = 100;
+    panel.SetActive(false);
+    InvokeRepeating("Move", 0.5f, 0.08f);
   }
 
   void Update() {
@@ -24,6 +32,7 @@ public class Snake : MonoBehaviour {
     if(Input.GetKey(KeyCode.UpArrow) && !direction.Equals(Vector2.down))
       direction = Vector2.up;
   }
+
   void Move() {
     Vector2 temp = transform.position;
     transform.Translate(direction);
@@ -41,8 +50,15 @@ public class Snake : MonoBehaviour {
 
   void OnTriggerEnter2D(Collider2D other) {
     if(other.gameObject.CompareTag("Food")) {
-      Destroy(other.gameObject);
+      points += plus;
+      showPoints.text = points.ToString();
       eat = true;
+    }
+
+    if(other.gameObject.CompareTag("Border") || 
+       other.gameObject.CompareTag("Tail")) {
+        panel.SetActive(true);
+        Time.timeScale = 0;
     }
   }
 }
